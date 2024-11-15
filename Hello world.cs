f8d9f1cf928bab25f376bcead68bd178e9828993
HiAndr31 <168109808+HiAndr31@users.noreply.github.com>
2024-11-15 18:56:55 +0200

    Crickets.cs

--- Hello world.cs
+++ Hello world.cs
@@ -7,12 +7,13 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("hello world!");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        //It'll spam circkets
         print("crickets");
     }
 }
